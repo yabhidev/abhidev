3134c25b9cadbb629c80be522c0db4d4201d4ff6
yAbhinaykumar <abhinayyalala007@gmail.com>
2021-10-25 18:09:21 +0530

    css code

---
+++ css/connect.php
@@ -0,0 +1,8 @@
+<?php
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$age = $_POST['age'];
+$rollnumber = $_POST['rollnumber'];
+$firstname = $_POST['firstname'];
+$firstname = $_POST['firstname'];
+?>
